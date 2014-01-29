a187a4fc5adf7bf0b7a1cd59aedd22b2cea3c96b
shimokitabenbot <shimokitaben@yahoo.co.jp>
2014-01-29 22:58:36 +0900

    fix

--- bin/bot_requester.rb
+++ bin/bot_requester.rb
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 require 'base64'
+require 'logger'
+require 'net/http'
 # BotのURL
-BOT_URL=ENV['BOT_URL']
-LOG_FILE_PATH = ENV['LOG_FILE_PATH']
+BOT_URL = ARGV[0]
+LOG_FILE_PATH = ARGV[1]
 LOG_FILE_NAME = "requester_#{Time.now.strftime('%Y-%m-%d')}.log"
 
 logger = Logger.new("#{LOG_FILE_PATH}/#{LOG_FILE_NAME}")
@@ -11,7 +13,9 @@ logger.info("URL = #{BOT_URL}")
 uri = URI.parse(BOT_URL)
 
 # Basic 認証
-BASIC_AUTH =  Base64::encode64("#{ENV['AUTH_USER']}:#{ENV['AUTH_PASS']}")
+AUTH_USER = ARGV[2]
+AUTH_PASS = ARGV[3]
+BASIC_AUTH =  Base64::encode64("#{AUTH_USER}:#{AUTH_PASS}")
 
 # HTTPリクエスト
 request = Net::HTTP::Post.new(uri.request_uri, initheader = {"Authorization" => "Basic #{BASIC_AUTH}"})
@@ -23,7 +27,8 @@ begin
   http.start do |h|
     response = h.request(request)
   end
-  if response.code == 201
+  logger.info("response.code = #{response.code}")
+  if response.code == "201"
     logger.info("Tweet Succeeded.")
     logger.info("Tweet content: #{response.body}")   
   else
--- bin/bot_requester.sh
+++ bin/bot_requester.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 # 必要なパラメータ
 # 環境変数に定義すること。
 # BOT_URL : BotのURL
 # LOG_FILE_PATH : ログ出力先フォルダ
 # AUTH_USER : 認証ユーザ
 # AUTH_PASS : 認証パスワード
-ruby bot_requester.rb
+echo $BOT_URL
+echo $LOG_FILE_PATH
+ruby ./bin/bot_requester.rb $BOT_URL $LOG_FILE_PATH $AUTH_USER $AUTH_PASS
